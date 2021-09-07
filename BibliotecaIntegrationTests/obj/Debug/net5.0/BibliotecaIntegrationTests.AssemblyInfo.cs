24a8b2eef6a24f544b286acdb7cd007406d7735e
Andrew Borges <andrewborges71@gmail.com>
2021-09-07 19:47:27 -0300

    Incluindo Testes com JMeter

--- BibliotecaIntegrationTests/obj/Debug/net5.0/BibliotecaIntegrationTests.AssemblyInfo.cs
+++ BibliotecaIntegrationTests/obj/Debug/net5.0/BibliotecaIntegrationTests.AssemblyInfo.cs
@@ -11,7 +11,7 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Biblioteca, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\Andrew\\Source\\Repos\\Biblioteca\\Biblioteca", "Biblioteca.csproj", "0")]
+[assembly: Microsoft.AspNetCore.Mvc.Testing.WebApplicationFactoryContentRootAttribute("Biblioteca, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", "C:\\Users\\Andrew\\source\\repos\\Biblioteca\\Biblioteca", "Biblioteca.csproj", "0")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("BibliotecaIntegrationTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
