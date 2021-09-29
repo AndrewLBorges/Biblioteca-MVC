c0a21761d700b8e255b06ea061ee027cc1f821ad
Andrew Borges <andrew.borges@esx.com.br>
2021-09-29 15:35:06 -0300

    adicionando script de terraform

---
+++ IAC/main.tf
@@ -0,0 +1,67 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 2.65"
+    }
+  }
+  required_version = ">= 0.12"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = "myTFResourceGroup"
+  location = "westus2"
+}
+
+resource "azurerm_app_service_plan" "biblio" {
+  name                = "biblio-appserviceplan-pro"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+resource "azurerm_app_service" "biblioteca" {
+  name                = "biblioteca-app-service"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  app_service_plan_id = azurerm_app_service_plan.biblio.id
+
+  site_config {
+    dotnet_framework_version = "v4.0"
+    scm_type                 = "LocalGit"
+  }
+}
+
+resource "azurerm_mssql_server" "biblio" {
+  name                         = "mssqlserver-biblio"
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
+  version                      = "12.0"
+  administrator_login          = "andrew"
+  administrator_login_password = "abCD1234"
+  minimum_tls_version          = "1.2"
+}
+
+resource "azurerm_mssql_firewall_rule" "rule" {
+  name             = "FirewallRule1"
+  server_id        = azurerm_mssql_server.biblio.id
+  start_ip_address = "0.0.0.0"
+  end_ip_address   = "0.0.0.0"
+}
+
+resource "azurerm_mssql_database" "biblio" {
+  name           = "biblioDB"
+  server_id      = azurerm_mssql_server.biblio.id
+  collation      = "SQL_Latin1_General_CP1_CI_AS"
+  license_type   = "LicenseIncluded"
+  max_size_gb    = 2
+  sku_name       = "Basic"
+}
